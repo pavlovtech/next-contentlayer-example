73bbddbf7bfa9dab71e110b3a043f2100a20c7aa
Alexander Pavlov <olpavlov@deloitte.com>
2023-05-12 20:50:50 +0300

    up

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { withContentlayer } = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: true, 
   },
 };
 
